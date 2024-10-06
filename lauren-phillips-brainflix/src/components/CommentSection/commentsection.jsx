3a09f0966fdb1bf831bf81bf996b586fb0f22f08
i-am-lp <lp.phillips95@gmail.com>
2024-10-06 11:11:24 -0400

    start of field form scss

--- lauren-phillips-brainflix/src/components/CommentSection/commentsection.jsx
+++ lauren-phillips-brainflix/src/components/CommentSection/commentsection.jsx
@@ -2,13 +2,13 @@ import './commentsection.scss';
 
 function CommentSection () {
     return (
-        <div className="main__comments">
-            <h2 className="main__comments--header">Join the Conversation</h2>
-            <div className="main__comments--form--box">
-                <div className="main__comments--image" alt="A close up profile picture of a man"></div>
-                <form id="main__comments--form" className="main__comments--form" method="post">
-                    <textarea name="comments" className="main__comments--form--placeholder main__comments--form--placeholder--comment" id="comments" placeholder="Add a new comment"></textarea>
-                    <button type="submit" className="main__comments--form--button">COMMENT</button>
+        <div className="comments">
+            <div className="comments__image" alt="A close up profile picture of a man"></div>
+            <div className="comments__box">
+                <p className="comments__box--header">JOIN THE CONVERSATION</p>
+                <form id="comments__box--form" className="comments__box--form" method="post">
+                    <textarea name="comments" className="comments__box--form--comment" id="comments" placeholder="Add a new comment"></textarea>
+                    <button type="submit" className="comments__box--form--button">COMMENT</button>
                 </form>
                 
             </div>
